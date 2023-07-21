1a84fdd46a178f69617b97fe7f77d4081542caad
LeaClover <plesen_14@mail.ru>
2023-07-21 19:28:34 +0300

    add new project hw1

---
+++ HomeWork1/script.js
@@ -0,0 +1,12 @@
+﻿console.log(`Задание 1`);
+let tempCelc = Number(prompt('Введите число температуры в Цельсиях: '));
+// let tempCelc = 123;
+let tempFar = (9 / 5) * tempCelc + 32;
+console.log(`Температура по Цельсию = ${tempCelc}
+Температура по Фаренгейту = ${tempFar}`);
+
+console.log(`Задание 2`);
+let userName = String(prompt('Введите ваше имя: '));
+// let userName = 'Lea';
+let adminName = userName;
+console.log(`Имя админа: ${adminName}`);
